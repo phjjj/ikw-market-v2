ed55c275a4b336a8f8e948e2112e8730c5839d92
phj <aaagowns@gmail.com>
2024-04-27 01:52:25 +0900

    003. App.js에 로그인 유무 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+
+import { useEffect } from "react";
+
+import { useSetRecoilState } from "recoil";
 import router from "./routes/router";
+import { kakaoIdAtom, userAtom } from "./recoil/user";
+import { getUserDoc } from "./lib/db/user";
+import { IUser } from "./types";
+
+const SESSION_TIMEOUT = 30 * 60 * 1000; // 30분
 
 function App() {
-  return (
-    <RecoilRoot>
-      <RouterProvider router={router} />
-    </RecoilRoot>
-  );
+  const setKakaoId = useSetRecoilState(kakaoIdAtom);
+  const setUser = useSetRecoilState(userAtom);
+
+  useEffect(() => {
+    const sessionStorageKakaoId = sessionStorage.getItem("kakaoIdRecoilPerist");
+    if (sessionStorageKakaoId) {
+      try {
+        const { kakaoId: persistedKakaoId, timestamp } = JSON.parse(
+          sessionStorageKakaoId,
+        );
+
+        const now = Date.now();
+        // 현재시간 - 만든시간 <= 30분
+        if (now - timestamp <= SESSION_TIMEOUT) {
+          setKakaoId(persistedKakaoId);
+          getUserDoc(persistedKakaoId)
+            .then((userDoc) => {
+              if (userDoc && Object.keys(userDoc).length !== 0) {
+                setUser(userDoc as IUser);
+              } else {
+                console.log("사용자 정보가 없습니다.");
+              }
+            })
+            .catch((error) => {
+              console.error("사용자 정보 가져오기 오류:", error);
+            });
+        } else {
+          sessionStorage.removeItem("kakaoIdRecoilPerist");
+        }
+      } catch (error) {
+        console.error("세션스토리지 파싱 에러:", error);
+        sessionStorage.removeItem("kakaoIdRecoilPerist");
+      }
+    }
+  }, []);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 
-import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
 
+import { useRecoilValue } from "recoil";
 import ProductList from "../../components/ProductList";
 import Title from "../../components/common/atoms/Title";
 import { dbService } from "../../firebase/config";
 import { IProductData } from "../../types";
+import { userAtom } from "../../recoil/user";
 
 function HomePage() {
-  const { state: user } = useLocation();
-
   const [products, setProducts] = useState<IProductData[]>([]);
-
+  const user = useRecoilValue(userAtom);
   console.log(user);
   // 실시간 업데이트 snapshot을 사용 할 경우 단점을 생각했을 때,
   // 상품이 많을 경우 HomePage 컴포넌트가 리렌더링이 자주 일어남
