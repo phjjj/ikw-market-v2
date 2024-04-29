817314c9c8abd6192687ce67cc193f06845008a5
phj <aaagowns@gmail.com>
2024-04-30 00:37:45 +0900

    006. 코드 정리

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,20 +3,14 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
 
-import { useRecoilValueLoadable } from "recoil";
 import ProductList from "../../components/ProductList";
 import Title from "../../components/common/atoms/Title";
 import { dbService } from "../../firebase/config";
 import { IProductData } from "../../types";
-import { userSelector } from "../../recoil/user";
 
 function HomePage() {
   const [products, setProducts] = useState<IProductData[]>([]);
 
-  const userDataLoadable = useRecoilValueLoadable(userSelector);
-  if (userDataLoadable.state === "hasValue") {
-    console.log(userDataLoadable.contents);
-  }
   // 실시간 업데이트 snapshot을 사용 할 경우 단점을 생각했을 때,
   // 상품이 많을 경우 HomePage 컴포넌트가 리렌더링이 자주 일어남
 
