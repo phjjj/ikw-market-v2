2b2fd4240af6e651c8d9cbd42592fbd66f3d167d
phj <aaagowns@gmail.com>
2024-05-03 22:38:34 +0900

    003. 홈화면에서 전체 상품 불러오기 추가
    - 전체 상품 불러오기 fetch
    - fetch후 allProducts에 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,27 @@
 import styled from "styled-components";
 
-import { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
-
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import ProductList from "../../components/ProductList";
 import Title from "../../components/common/atoms/Title";
-import { dbService } from "../../firebase/config";
-import { IProductData } from "../../types";
+import { allProductsAtom } from "../../recoil/user";
+import { getAllProducts } from "../../lib/db/product";
 
 function HomePage() {
-  const [products, setProducts] = useState<IProductData[]>([]);
-
-  // 실시간 업데이트 snapshot을 사용 할 경우 단점을 생각했을 때,
-  // 상품이 많을 경우 HomePage 컴포넌트가 리렌더링이 자주 일어남
+  const [allProducts, setAllProducts] = useRecoilState(allProductsAtom);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(
-      collection(dbService, "products"),
-      (snapshot) => {
-        const productsData = snapshot.docs.map((doc) => doc.data());
-        setProducts(productsData as IProductData[]);
-      },
-    );
-
-    return () => unsubscribe();
-  }, []);
+    async function fetchData() {
+      const allProductsData = await getAllProducts();
+      setAllProducts(allProductsData);
+    }
+    fetchData();
+  }, [setAllProducts]);
 
   return (
     <HomePageWrapper>
       <Title className="xl">최근 중고거래 매물</Title>
-      <ProductList productData={products} />
+      <ProductList productData={allProducts} />
     </HomePageWrapper>
   );
 }
