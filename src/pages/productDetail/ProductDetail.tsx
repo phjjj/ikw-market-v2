c5435824ea9dd31cd7985d4cbd24c94ca3daba72
phj <aaagowns@gmail.com>
2024-05-03 22:42:11 +0900

    006. 상품 상세 정보 fetch 코드 추가
    - 전체 상품 목록(allProdctsAtom)이 있을 경우, 해당 state에서 가져옴
    
    - allProdctsAtom이 없을 경우 (새로고침 or 상세 정보 페이지로 바로 왔을 경우) fetch 실행

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,58 +1,73 @@
 /* eslint-disable react/button-has-type */
 import styled from "styled-components";
-
+import { useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import ProductInfo from "./ProductInfo/ProductInfo";
 import UserName from "./UserName/UserName";
 import Image from "./ProductInfo/molecules/ImageSlide";
-import CommentList from "./CommentList/CommentLIst";
+// import CommentList from "./CommentList/CommentLIst";
+
+import { IProductData } from "../../types";
+import { getProduct } from "../../lib/db/product";
+import { allProductsAtom } from "../../recoil/user";
 
 function ProductDetail() {
-  const DUMMY_PRODUCT_DATA = {
-    productId: 1,
-    images: [
-      "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
-      "https://cdnweb01.wikitree.co.kr/webdata/editor/202210/17/img_20221017153113_ca91465f.webp",
-    ],
-    title: "사진기",
-    price: 100000,
-    location: "2호관",
-    description:
-      "사진기팝니다.사진기팝니다.사사진기팝니다.사진기팝니다.사사진기팝니다.사진기팝니다.사사진기팝니다.사진기팝니다.사사진기팝니다.사진기팝니다.사사진기팝니다.사진기팝니다.사",
-    name: "카리나",
-    userId: 2,
-    comment: [
-      {
-        username: "윈터",
-        userId: 1,
-        userImage:
-          "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2024/02/20/ebb2c0d9-290d-4505-9d6e-5ee7ea2eec2c.jpg",
-        text: "구매원합니다",
-        date: "2024.03.31. 09:00",
-      },
-      {
-        username: "카리나",
-        userId: 2,
-        userImage:
-          "https://cdnweb01.wikitree.co.kr/webdata/editor/202210/17/img_20221017153113_ca91465f.webp",
-        text: "연락주세요",
-        date: "2024.03.31. 09:00",
-      },
-    ],
-    like: 10,
-  };
+  const allProducts = useRecoilValue(allProductsAtom);
+  // console.log(allProducts);
+  const [product, setProduct] = useState<IProductData | null>(null);
+
+  const location = useLocation();
+  const productId = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    if (allProducts.length) {
+      const productData = allProducts.find((p) => p.id === productId);
+      if (productData) {
+        setProduct(productData);
+      }
+    } else {
+      const fetchData = async () => {
+        const productData = await getProduct(productId);
+        setProduct(productData);
+      };
+      fetchData();
+    }
+  }, [allProducts, productId]);
+
+  // useEffect(() => {
+  //   // productsLoadable 값이 유효할 경우, fetch x
+  //   if (productsLoadable.state === "hasValue") {
+  //     setProduct(productsLoadable.contents);
+  //   } else if (productsLoadable.state === "hasError") {
+  //     // 값이 없을경우, 새로고침 하거나 링크로 바로 들어 올 경우
+  //     const fetchData = async () => {
+  //       const productData = await getProduct(productId);
+  //       setProduct(productData);
+  //     };
+
+  //     fetchData();
+  //   }
+  // }, [productsLoadable]);
 
   return (
     <ProductDetailWrapper>
-      <Image images={DUMMY_PRODUCT_DATA.images} />
-      <UserName body={DUMMY_PRODUCT_DATA.name} />
-      <hr style={{ width: "100%", margin: "0" }} />
-      <ProductInfo>
-        <ProductInfo.Name body={DUMMY_PRODUCT_DATA.title} />
-        <ProductInfo.Price body={DUMMY_PRODUCT_DATA.price} />
-        <ProductInfo.Location body={DUMMY_PRODUCT_DATA.location} />
-        <ProductInfo.Description body={DUMMY_PRODUCT_DATA.description} />
-      </ProductInfo>
-      <CommentList commentData={DUMMY_PRODUCT_DATA.comment} />
+      {!product ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Image images={product.images} />
+          <UserName body={product.userName} />
+          <hr style={{ width: "100%", margin: "0" }} />
+          <ProductInfo>
+            <ProductInfo.Name body={product.title} />
+            <ProductInfo.Price body={product.price} />
+            <ProductInfo.Location body={product.location} />
+            <ProductInfo.Description body={product.description} />
+          </ProductInfo>
+          {/* <CommentList commentData={product.commentList} /> */}
+        </>
+      )}
     </ProductDetailWrapper>
   );
 }
@@ -66,4 +81,5 @@ const ProductDetailWrapper = styled.main`
   margin: 0 auto;
   padding-bottom: 10rem;
 `;
+
 export default ProductDetail;
